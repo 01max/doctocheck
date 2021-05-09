5aaa7030a76a6b2ed069db801d1de9495641ba3b
mlouguet <mlouguet@trustpair.fr>
2021-05-09 16:42:27 +0200

    Add doctolib availabilities comparison & discord barebone interractions.

--- lib/docto_lib/compare_availabilities.rb
+++ lib/docto_lib/compare_availabilities.rb
@@ -3,7 +3,7 @@
 module DoctoLib
   # Availabilities comparison
   class CompareAvailabilities
-    attr_reader :result_hash
+    attr_reader :result_hash, :current_resume
 
     def initialize(current_hash:, previous_hash: {})
       @current_hash = current_hash
@@ -14,24 +14,29 @@ module DoctoLib
 
     def process
       @result_hash = {}
+      @current_resume = {}
 
       compare_totals
       compare_next_slots
       compare_date_slots
+
+      result_hash[:current_resume] = current_resume
     end
 
     private
 
     attr_reader :current_hash, :previous_hash
-    attr_writer :result_hash
+    attr_writer :result_hash, :current_resume
 
     def compare_totals
+      current_resume[:total] = current_hash['total']
       return unless current_hash['total'] != previous_hash['total']
 
       result_hash[:total_diff] = current_hash['total'] - previous_hash['total']
     end
 
     def compare_next_slots
+      current_resume[:next_slot] = current_hash['next_slot']
       return unless current_hash['next_slot']
       return unless current_hash['next_slot'] != previous_hash['next_slot']
 
@@ -40,12 +45,15 @@ module DoctoLib
 
     def compare_date_slots
       result_hash[:slots_count_diff] = {}
+      current_resume[:slots_count] = {}
 
       current_hash['availabilities'].each do |current_date_availabilities|
         date = current_date_availabilities['date']
         previous_date_availabilities = previous_hash['availabilities'].select { |avb| avb['date'] == date }.first
         previous_slots_count = previous_date_availabilities['slots'].to_a.length
         current_slots_count = current_date_availabilities['slots'].to_a.length
+
+        current_resume[:slots_count][date] = current_slots_count
 
         next unless current_slots_count != previous_slots_count
 
--- main.rb
+++ main.rb
@@ -1,20 +1,39 @@
 # frozen_string_literal: true
 
+require 'dotenv'
+
+Dotenv.load
+
+require 'discordrb'
 require_relative 'lib/docto_lib'
 
-availabilities = DoctoLib::RetrieveAvailabilities.new(
-  start_date: Date.today,
-  visit_motive_ids: %w[2542516],
-  agenda_ids: %w[445302 457249 413635 426805 464787]
-)
+def run_check(force_output: false)
+  availabilities = DoctoLib::RetrieveAvailabilities.new(
+    start_date: Date.today,
+    visit_motive_ids: %w[2542516],
+    agenda_ids: %w[445302 457249 413635 426805 464787]
+  )
 
-unless availabilities.previous_hash.empty?
-  compare_results = DoctoLib::CompareAvailabilities.new(
-    current_hash: availabilities.raw_hash,
-    previous_hash: availabilities.previous_hash
-  ).result_hash
+  unless availabilities.previous_hash.empty?
+    compare_results = DoctoLib::CompareAvailabilities.new(
+      current_hash: availabilities.raw_hash,
+      previous_hash: availabilities.previous_hash
+    ).result_hash
+  end
+
+  return if compare_results.except(:current_resume).empty? && !force_output
+
+  bot_message = compare_results.map do |key, value|
+    displayed_value = value.is_a?(Hash) ? "\n`#{value}`" : " : #{value}"
+    "__**#{key.capitalize}**__#{displayed_value}"
+  end.join("\n")
+
+  bot = Discordrb::Bot.new(token: ENV['DISCORD_BOT_TOKEN'])
+  bot.run(true)
+  bot.send_message(ENV['DISCORD_CHANNEL_ID'], "@everyone \n#{bot_message}")
+  bot.stop
+
+  availabilities.archive!
 end
 
-puts "compare_results : #{compare_results}"
-
-# availabilities.archive!
+run_check(force_output: true)
